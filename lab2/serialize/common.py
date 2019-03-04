f9d462e079ab65aa684680b0c20069c53dc8b3b7
Volodymyr Kravchuk <vantuyyya@gmail.com>
2019-03-04 23:23:27 +0200

    add configurations file and use configparser

--- lab2/serialize/common.py
+++ lab2/serialize/common.py
@@ -1,24 +1,30 @@
 from serialize import pickle_serialize
 from serialize import yaml_serialize
 from serialize import json_serialize
-serialization_type = 1
+from configs import config_reader
 
 
 def save_user(user):
     """common procedure to serialize by given type"""
+    config = config_reader.read_config()
+    serialization_type = int(config.get('Serialization', 'type'))
+    path = config.get('Serialization', 'path')
     if serialization_type == 1:
-        pickle_serialize.save(user, "../user-pickle")
+        pickle_serialize.save(user, path)
     elif serialization_type == 2:
-        yaml_serialize.save(user, "../user-yaml")
+        yaml_serialize.save(user, path)
     elif serialization_type == 3:
-        json_serialize.save(user, "../user-json")
+        json_serialize.save(user, path)
 
 
 def load_user():
     """common deserialize function"""
+    config = config_reader.read_config()
+    serialization_type = int(config.get('Serialization', 'type'))
+    path = config.get('Serialization', 'path')
     if serialization_type == 1:
-        return pickle_serialize.load("../user-pickle")
+        return pickle_serialize.load(path)
     elif serialization_type == 2:
-        return yaml_serialize.load("../user-yaml")
+        return yaml_serialize.load(path)
     elif serialization_type == 3:
-        return json_serialize.load("../user-json")
+        return json_serialize.load(path)
